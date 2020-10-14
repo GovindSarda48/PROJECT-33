c25e5caf5951094226f5643b293cb592d77cb90a
GovindSarda48 <70316473+GovindSarda48@users.noreply.github.com>
2020-10-14 12:35:23 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true,
+          
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.color = color(random(0,255),random(0,255),random(0,255));
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      
+      noStroke();
+      //strokeWeight(4);
+      fill(this.color);
+      rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  }
---
+++ sketch.js
@@ -0,0 +1,184 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Events = Matter.Events;
+
+var divisions = [];
+//var particles = [];
+var plinkos = [];
+
+var divisionHeight = 300;
+
+var score =0;
+var turn = 0;
+
+var PLAY =1;
+
+var END=0;
+var particles;
+var gameState = PLAY;
+var check=0
+
+function setup() {
+
+  engine = Engine.create();
+  world = engine.world;
+  createCanvas(500,800);
+
+  ground1 = new Ground(240,790,800,20);
+  s1 = new Box(50,655,65,250);
+  s2 = new Box(130,655,65,250);
+  s3 = new Box(210,655,65,250);
+  s4 = new Box(290,655,65,250);
+  s5 = new Box(370,655,65,250);
+  s6 = new Box(450,655,65,250);
+
+  for(var k=10;k<=width;k=k+80){
+    divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight));
+  }
+
+  for(var j=15;j<=width;j=j+40){
+    plinkos.push(new Plinko(j,40,20));
+  }
+
+  for(var j=20;j<=width;j=j+50){
+    plinkos.push(new Plinko(j,80,20));
+  }
+
+
+  for(var j=15;j<=width;j=j+40){
+    plinkos.push(new Plinko(j,120,20));
+  }
+
+  for(var j=20;j<=width;j=j+50){
+    plinkos.push(new Plinko(j,160,20));
+  }
+
+  for(var j=15;j<=width;j=j+40){
+    plinkos.push(new Plinko(j,200,20));
+  }
+  
+  
+
+  //Engine.update(engine);
+}
+
+function draw() {
+  background(0); 
+  
+  console.log(turn);
+
+  s1.display();
+  s2.display();
+  s3.display();
+  s4.display();
+  s5.display();
+  s6.display();
+  ground1.display();
+  
+  Engine.update(engine); 
+
+  fill("white");
+  stroke("red");
+  strokeWeight(4);
+  textSize(20);
+  text("Score : "+score,20,30);
+  textSize(25);
+  text("500 ",30,525);
+  text("200 ",110,525);
+  text("400 ",190,525);
+  text("150 ",270,525);
+  text("600 ",350,525);
+  text("250 ",430,525);
+
+
+  
+ 
+  
+  for(var k = 0;k<divisions.length;k++){
+    divisions[k].display();
+  }
+
+  for(var j = 0;j<plinkos.length;j++){
+    plinkos[j].display();
+  }
+  
+  
+  
+  //var particle;
+ /*if(frameCount % 50===0){
+    particle = new Particle(random(120,380),10,20);
+    //particles1.push(particle);
+  }
+*/
+
+  if(particles !== null && check!==0){
+    console.log(particles)
+    particles.display();
+    if(particles.body.position.y>500){
+    console.log(particles.body.position.x)
+      if(particles.body.position.x<90){
+        score = score+500;
+      // particles =null;
+      
+      }
+      if(particles.body.position.x>90 && particles.body.position.x<170 ){
+        score = score+200;
+        //particles =null;
+      }
+      if(particles.body.position.x>170 && particles.body.position.x<250 ){
+        score = score+400;
+        //particles = null;
+      }
+      if(particles.body.position.x>250 && particles.body.position.x<330 ){
+        score = score+150;
+      // particles = null;
+      }
+      if(particles.body.position.x>330 && particles.body.position.x<410 ){
+        score = score+600;
+        //particles = null;
+      
+      }
+      if(particles.body.position.x>410 && particles.body.position.x<490 ){
+        score = score+250;
+        //particles = null;
+      }
+      particles = null;
+    }
+   
+         
+
+  }
+  if( turn === 5)
+  {
+    //console.log("Hi");
+   gameState = END
+   textSize(70); 
+   text("GAME OVER",25,250) 
+  }
+
+  
+}
+
+function mousePressed(){
+
+check=1
+
+  if(gameState!==END ){
+    console.log(gameState);
+    //particle = new Particle(mouseX,10,20);
+    particles = new Particle(mouseX,10,20);
+    console.log(particles);
+    //particles1.push(particle);
+    turn++;
+
+   /*for(var m= 0;  m<particles1.length ;m++){
+      particles1[m].display();
+      console.log(particles1[m]);
+    }*/
+  }
+
+ 
+  
+}
